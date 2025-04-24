18cefd9c3f811462bc36d07fa2f09293a2280a43
Nobutora Iyoda <nobysemail@gmail.com>
2025-04-23 20:32:17 -0400

    Add js file to dynamically display game links

---
+++ index.js
@@ -0,0 +1,25 @@
+// Example data
+const imageLinks = [
+    {
+      url: "https://example.com/page1",
+      img: "assets/logo.png",
+      alt: "Example 1"
+    },
+    {
+      url: "https://example.com/page2",
+      img: "assets/logo.png",
+      alt: "Example 2"
+    },
+    {
+      url: "https://example.com/page3",
+      img: "assets/logo.png",
+      alt: "Example 3"
+    }
+];
+
+const container = document.querySelector('#linked-games');
+
+// imageLinks.forEach(({ url, img, alt }) => {
+//     const link = 
+// });
+
