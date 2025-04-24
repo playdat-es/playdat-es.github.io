1d3d84a15bf385a41792f347e0eb8221c1d196b6
Nobutora Iyoda <nobysemail@gmail.com>
2025-04-24 03:40:58 -0400

    add spyfall logo and create style linked images as cards

--- index.js
+++ index.js
@@ -1,39 +1,51 @@
 // Example data
-const imageLinks = [
+const games = [
     {
       url: 'https://example.com/page1',
-      img: 'assets/logo.png',
-      alt: 'Spyfall'
+      img: 'assets/spyfall_logo.svg',
+      title: 'Spyfall'
     },
     {
         url: 'https://example.com/page1',
         img: 'assets/logo.png',
-        alt: 'Logo 1'
+        title: 'Logo 1'
     },
     {
         url: 'https://example.com/page1',
         img: 'assets/logo.png',
-        alt: 'Logo 2'
+        title: 'Logo 2'
     },
 ];
 
-const container = document.querySelector('#linked-games');
+const gridContainer = document.querySelector('#linked-games');
 
-imageLinks.forEach(({ url, img, alt }) => {
+const createGameCard = ({ url, img, title }) => {
     const link = document.createElement("a");
     link.href = url;
+    link.style = 'max-width: 250px;';
+
+    const card = document.createElement('div');
+    card.className = 'card';
+
+    const cardContent = document.createElement('content');
+    cardContent.className = 'content u-text-center';
 
     const image = document.createElement('img');
     image.src = img;
-    image.alt = alt;
-    image.className = 'img-responsive u-shadow-sm';
-    image.style.maxWidth = '200px';
-    image.style.height = 'auto';
+    image.alt = title;
+    image.className = 'img-responsive bg-dark';
 
-    const div = document.createElement('div');
-    div.className = 'u-flex u-justify-center';
-    link.appendChild(image);
-    div.append(link)
-    container.appendChild(div);
+    const p = document.createElement('p');
+    p.className = 'title';
+    p.textContent = title;
+
+    cardContent.appendChild(image);
+    cardContent.appendChild(p);
+    card.appendChild(cardContent);
+    link.appendChild(card);
+    gridContainer.appendChild(link);
+};
+
+games.forEach(game => {
+    createGameCard(game);
 });
-
