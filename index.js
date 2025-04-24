a161319f382e43ca2bf1ee6f8a28e793f24ed7d9
Nobutora Iyoda <nobysemail@gmail.com>
2025-04-23 21:07:48 -0400

    Dynamically create links to pages

--- index.js
+++ index.js
@@ -1,25 +1,36 @@
 // Example data
 const imageLinks = [
     {
-      url: "https://example.com/page1",
-      img: "assets/logo.png",
-      alt: "Example 1"
+      url: 'https://example.com/page1',
+      img: 'assets/logo.png',
+      alt: 'Spyfall'
     },
     {
-      url: "https://example.com/page2",
-      img: "assets/logo.png",
-      alt: "Example 2"
+        url: 'https://example.com/page1',
+        img: 'assets/logo.png',
+        alt: 'Logo 1'
     },
     {
-      url: "https://example.com/page3",
-      img: "assets/logo.png",
-      alt: "Example 3"
-    }
+        url: 'https://example.com/page1',
+        img: 'assets/logo.png',
+        alt: 'Logo 2'
+    },
 ];
 
 const container = document.querySelector('#linked-games');
 
-// imageLinks.forEach(({ url, img, alt }) => {
-//     const link = 
-// });
+imageLinks.forEach(({ url, img, alt }) => {
+    const link = document.createElement("a");
+    link.href = url;
+
+    const image = document.createElement('img');
+    image.src = img;
+    image.alt = alt;
+    image.className = 'img-responsive u-shadow-sm';
+    image.style.width = '150px';
+    image.style.height = 'auto';
+
+    link.appendChild(image);
+    container.appendChild(link);
+});
 
